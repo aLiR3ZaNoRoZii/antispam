ca76ba29d366a4a2ca051eabc61eaf56932f04c0
aLiR3ZaNoRoZii <32227171+aLiR3ZaNoRoZii@users.noreply.github.com>
2017-09-23 22:30:32 +0330

    Update config.lua

--- data/config.lua
+++ data/config.lua
@@ -1,1 +1,44 @@
-
+do local _ = {
+  admins = {
+    {
+      385190735,
+      "@Alibastan5"
+    },
+    {
+      169762521,
+      "@hesamamir20"
+    },
+    {
+      444369211,
+      "@Baran\\_ravanii"
+    },
+    {
+      153668929,
+      "@Gelayew"
+    },
+    {
+      358287474,
+      "@Meehran\\_tag"
+    }
+  },
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "groupmanager",
+    "msg-checks",
+    "plugins",
+    "tools",
+    "delall"
+  },
+  info_text = "》alireza Reborn v6.0\nAn advanced administration bot\n\n \n\n》Admins :\n》@aLiR3ZaNoRoZi ➣ Founder & Developer《\n》@Sorushnriteam ➣ Developer《\n》@Ass90 ➣ Developer《\n\n》Special thanks to :\n》alireza Team Members\n\n》Our channel :\n》@UNIQUEMOVIEE《\n\n》Our website :\n》http://alinri.ir\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    364464502,
+    440461830,
+    152158957
+  }
+}
+return _
+end
